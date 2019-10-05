4d674ea628de1dbdd4ca319c23d808115e412930
ღஐ Monique Sullivan ღஐ <bmoniquesullivan@gmail.com>
2019-10-04 21:27:58 -0400

    Craps Game

---
+++ Craps/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Craps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSum = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.lblNewGame = new System.Windows.Forms.Label();
+            this.lblDisplayMsg = new System.Windows.Forms.Label();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblInstr2 = new System.Windows.Forms.Label();
+            this.lblInstr = new System.Windows.Forms.Label();
+            this.btnInstr = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDisplayResult = new System.Windows.Forms.Label();
+            this.picDiceNum2 = new System.Windows.Forms.PictureBox();
+            this.picDiceNum3 = new System.Windows.Forms.PictureBox();
+            this.picDiceNum4 = new System.Windows.Forms.PictureBox();
+            this.picDiceNum5 = new System.Windows.Forms.PictureBox();
+            this.picDiceNum6 = new System.Windows.Forms.PictureBox();
+            this.picDiceNum1 = new System.Windows.Forms.PictureBox();
+            this.picDice2 = new System.Windows.Forms.PictureBox();
+            this.picDice1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDice2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDice1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(334, 335);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(40, 17);
+            this.lblSum.TabIndex = 29;
+            this.lblSum.Text = "Sum:";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(88, 290);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(103, 33);
+            this.btnNewGame.TabIndex = 28;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // lblNewGame
+            // 
+            this.lblNewGame.AutoSize = true;
+            this.lblNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewGame.Location = new System.Drawing.Point(30, 255);
+            this.lblNewGame.Name = "lblNewGame";
+            this.lblNewGame.Size = new System.Drawing.Size(240, 20);
+            this.lblNewGame.TabIndex = 27;
+            this.lblNewGame.Text = "Click here to play a New Game";
+            // 
+            // lblDisplayMsg
+            // 
+            this.lblDisplayMsg.AutoSize = true;
+            this.lblDisplayMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayMsg.Location = new System.Drawing.Point(530, 162);
+            this.lblDisplayMsg.Name = "lblDisplayMsg";
+            this.lblDisplayMsg.Size = new System.Drawing.Size(0, 20);
+            this.lblDisplayMsg.TabIndex = 26;
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Location = new System.Drawing.Point(334, 309);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(51, 17);
+            this.lblPoints.TabIndex = 25;
+            this.lblPoints.Text = "Points:";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(326, 245);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(122, 49);
+            this.btnPlay.TabIndex = 24;
+            this.btnPlay.Text = "PLAY";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblInstr2
+            // 
+            this.lblInstr2.AutoSize = true;
+            this.lblInstr2.Font = new System.Drawing.Font("Chiller", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstr2.Location = new System.Drawing.Point(220, 94);
+            this.lblInstr2.Name = "lblInstr2";
+            this.lblInstr2.Size = new System.Drawing.Size(348, 46);
+            this.lblInstr2.TabIndex = 21;
+            this.lblInstr2.Text = "Do you want to play Craps?";
+            // 
+            // lblInstr
+            // 
+            this.lblInstr.AutoSize = true;
+            this.lblInstr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstr.Location = new System.Drawing.Point(18, 156);
+            this.lblInstr.Name = "lblInstr";
+            this.lblInstr.Size = new System.Drawing.Size(261, 20);
+            this.lblInstr.TabIndex = 20;
+            this.lblInstr.Text = "Click here to read the Instructions";
+            // 
+            // btnInstr
+            // 
+            this.btnInstr.Location = new System.Drawing.Point(88, 191);
+            this.btnInstr.Name = "btnInstr";
+            this.btnInstr.Size = new System.Drawing.Size(103, 33);
+            this.btnInstr.TabIndex = 19;
+            this.btnInstr.Text = "Instructions";
+            this.btnInstr.UseVisualStyleBackColor = true;
+            this.btnInstr.Click += new System.EventHandler(this.btnInstr_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Chiller", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(310, 11);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(173, 92);
+            this.lblTitle.TabIndex = 18;
+            this.lblTitle.Text = "Craps";
+            // 
+            // lblDisplayResult
+            // 
+            this.lblDisplayResult.AutoSize = true;
+            this.lblDisplayResult.Font = new System.Drawing.Font("Chiller", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayResult.Location = new System.Drawing.Point(309, 403);
+            this.lblDisplayResult.Name = "lblDisplayResult";
+            this.lblDisplayResult.Size = new System.Drawing.Size(0, 33);
+            this.lblDisplayResult.TabIndex = 36;
+            // 
+            // picDiceNum2
+            // 
+            this.picDiceNum2.Image = global::Craps.Properties.Resources.two;
+            this.picDiceNum2.Location = new System.Drawing.Point(59, 403);
+            this.picDiceNum2.Name = "picDiceNum2";
+            this.picDiceNum2.Size = new System.Drawing.Size(35, 37);
+            this.picDiceNum2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDiceNum2.TabIndex = 35;
+            this.picDiceNum2.TabStop = false;
+            // 
+            // picDiceNum3
+            // 
+            this.picDiceNum3.Image = global::Craps.Properties.Resources.three;
+            this.picDiceNum3.Location = new System.Drawing.Point(100, 403);
+            this.picDiceNum3.Name = "picDiceNum3";
+            this.picDiceNum3.Size = new System.Drawing.Size(35, 37);
+            this.picDiceNum3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDiceNum3.TabIndex = 34;
+            this.picDiceNum3.TabStop = false;
+            // 
+            // picDiceNum4
+            // 
+            this.picDiceNum4.Image = global::Craps.Properties.Resources.four;
+            this.picDiceNum4.Location = new System.Drawing.Point(141, 403);
+            this.picDiceNum4.Name = "picDiceNum4";
+            this.picDiceNum4.Size = new System.Drawing.Size(35, 37);
+            this.picDiceNum4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDiceNum4.TabIndex = 33;
+            this.picDiceNum4.TabStop = false;
+            // 
+            // picDiceNum5
+            // 
+            this.picDiceNum5.Image = global::Craps.Properties.Resources.five;
+            this.picDiceNum5.Location = new System.Drawing.Point(182, 403);
+            this.picDiceNum5.Name = "picDiceNum5";
+            this.picDiceNum5.Size = new System.Drawing.Size(35, 37);
+            this.picDiceNum5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDiceNum5.TabIndex = 32;
+            this.picDiceNum5.TabStop = false;
+            // 
+            // picDiceNum6
+            // 
+            this.picDiceNum6.Image = global::Craps.Properties.Resources.six;
+            this.picDiceNum6.Location = new System.Drawing.Point(223, 403);
+            this.picDiceNum6.Name = "picDiceNum6";
+            this.picDiceNum6.Size = new System.Drawing.Size(35, 37);
+            this.picDiceNum6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDiceNum6.TabIndex = 31;
+            this.picDiceNum6.TabStop = false;
+            // 
+            // picDiceNum1
+            // 
+            this.picDiceNum1.Image = global::Craps.Properties.Resources.one;
+            this.picDiceNum1.Location = new System.Drawing.Point(18, 403);
+            this.picDiceNum1.Name = "picDiceNum1";
+            this.picDiceNum1.Size = new System.Drawing.Size(35, 37);
+            this.picDiceNum1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDiceNum1.TabIndex = 30;
+            this.picDiceNum1.TabStop = false;
+            // 
+            // picDice2
+            // 
+            this.picDice2.Location = new System.Drawing.Point(427, 162);
+            this.picDice2.Name = "picDice2";
+            this.picDice2.Size = new System.Drawing.Size(80, 71);
+            this.picDice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDice2.TabIndex = 23;
+            this.picDice2.TabStop = false;
+            // 
+            // picDice1
+            // 
+            this.picDice1.Location = new System.Drawing.Point(326, 162);
+            this.picDice1.Name = "picDice1";
+            this.picDice1.Size = new System.Drawing.Size(85, 71);
+            this.picDice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDice1.TabIndex = 22;
+            this.picDice1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDisplayResult);
+            this.Controls.Add(this.picDiceNum2);
+            this.Controls.Add(this.picDiceNum3);
+            this.Controls.Add(this.picDiceNum4);
+            this.Controls.Add(this.picDiceNum5);
+            this.Controls.Add(this.picDiceNum6);
+            this.Controls.Add(this.picDiceNum1);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblNewGame);
+            this.Controls.Add(this.lblDisplayMsg);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.picDice2);
+            this.Controls.Add(this.picDice1);
+            this.Controls.Add(this.lblInstr2);
+            this.Controls.Add(this.lblInstr);
+            this.Controls.Add(this.btnInstr);
+            this.Controls.Add(this.lblTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Play Craps";
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDiceNum1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDice2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDice1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDiceNum2;
+        private System.Windows.Forms.PictureBox picDiceNum3;
+        private System.Windows.Forms.PictureBox picDiceNum4;
+        private System.Windows.Forms.PictureBox picDiceNum5;
+        private System.Windows.Forms.PictureBox picDiceNum6;
+        private System.Windows.Forms.PictureBox picDiceNum1;
+        private System.Windows.Forms.Label lblSum;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label lblNewGame;
+        private System.Windows.Forms.Label lblDisplayMsg;
+        private System.Windows.Forms.Label lblPoints;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.PictureBox picDice2;
+        private System.Windows.Forms.PictureBox picDice1;
+        private System.Windows.Forms.Label lblInstr2;
+        private System.Windows.Forms.Label lblInstr;
+        private System.Windows.Forms.Button btnInstr;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDisplayResult;
+    }
+}
+
---
+++ Craps/Form1.cs
@@ -0,0 +1,189 @@
+﻿/***************************************************************
+* Student: Barbara Sullivan                                    *
+* Instructor: Dr. Jerry Mamo                                   *
+* Class: CIST 2341 – C# Programming – 60485 – Summer 2019      * 
+* Mini-Project - Craps Game                                    *
+****************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Craps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Random randomNumber = new Random();
+
+        //Enumerators to represent Game Status
+        private enum Status  
+        {
+            CONTINUE,
+            WON,
+            LOST
+        }
+        private enum DiceNames
+        {
+            SNAKE_EYES = 2,
+            TREY = 3,
+            SEVEN = 7,
+            YO_LEVEN = 11,
+            BOX_CARS = 12
+        }
+        // PLAY ONE GAME OF CRAP  
+        public void Play()
+        {
+            Status gameStatus = Status.CONTINUE;
+            int myPoint = 0;
+
+            //Show the number of Points on Points Label
+            lblPoints.Text = ("Points: " + myPoint.ToString());
+
+            //Roll Dices
+            int sumOfDice = RollDice();
+
+            //Determine the results Win/Lose
+            switch ((DiceNames)sumOfDice)
+            {
+                case DiceNames.SEVEN:
+                case DiceNames.YO_LEVEN:
+                    gameStatus = Status.WON;
+                    break;
+                case DiceNames.BOX_CARS:
+                case DiceNames.SNAKE_EYES:
+                case DiceNames.TREY:
+                    gameStatus = Status.LOST;
+                    break;
+                default:
+                    gameStatus = Status.CONTINUE;
+                    myPoint = sumOfDice;
+                    lblPoints.Text = ("Points: " + myPoint.ToString());
+                    Console.WriteLine("Point is {0}", myPoint);
+                    break;
+
+            }
+            while (gameStatus == Status.CONTINUE)
+            {
+                sumOfDice = RollDice(); // roll dice again  
+                if (sumOfDice == myPoint)
+                    gameStatus = Status.WON;
+                if (sumOfDice == (int)DiceNames.SEVEN)
+                    gameStatus = Status.LOST;
+            }
+            // end of while method  
+            // Display won or loss  
+            if (gameStatus == Status.WON)
+            {
+                lblDisplayResult.Text = "You Win";
+                Console.WriteLine("You Win");
+            }
+            else
+            {
+                lblDisplayResult.Text = "You Lose";
+                Console.WriteLine("You Lose");
+            }
+        }//end of method Play  
+
+        public int RollDice()
+        {
+            //Assign Random numbers to Dices and Dice images according to numbers
+            int die1 = new int();
+            die1 = randomNumber.Next(1, 7);
+
+            //Assign Dice 1 Image
+            if (die1 == 1)
+            {
+                picDice1.Image = picDiceNum1.Image;
+                picDice1.Visible = true;
+            }
+            else if (die1 == 2)
+            {
+                picDice1.Image = picDiceNum2.Image;
+                picDice1.Visible = true;
+            }
+            else if (die1 == 3)
+            {
+                picDice1.Image = picDiceNum3.Image;
+                picDice1.Visible = true;
+            }
+            else if (die1 == 4)
+            {
+                picDice1.Image = picDiceNum4.Image;
+                picDice1.Visible = true;
+            }
+            else if (die1 == 5)
+            {
+                picDice1.Image = picDiceNum5.Image;
+                picDice1.Visible = true;
+            }
+            else if (die1 == 6)
+            {
+                picDice1.Image = picDiceNum6.Image;
+                picDice1.Visible = true;
+            }
+
+            int die2 = new int();
+            die2 = randomNumber.Next(1, 7);
+
+            //Assign Dice 2 Image
+            if (die2 == 1)
+                picDice2.Image = picDiceNum1.Image;
+            else if (die2 == 2)
+                picDice2.Image = picDiceNum2.Image;
+            else if (die2 == 3)
+                picDice2.Image = picDiceNum3.Image;
+            else if (die2 == 4)
+                picDice2.Image = picDiceNum4.Image;
+            else if (die2 == 5)
+                picDice2.Image = picDiceNum5.Image;
+            else if (die2 == 6)
+                picDice2.Image = picDiceNum6.Image;
+            int sum = die1 + die2;
+            lblSum.Text = ("Sum: " + sum.ToString());
+
+            //Display result of the rolls
+            String printSum = ("Player rolled " + die1.ToString() + " + " + die2.ToString() + " = " + sum.ToString());
+            lblDisplayMsg.Text += (printSum + System.Environment.NewLine);
+            Console.WriteLine("Player rolled {0} + {1} = {2}", die1, die2, sum);
+            return sum;
+
+        }// End method roll dice  
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            //Play the game when user clicks on Play
+            Play();
+        }
+
+        private void btnInstr_Click(object sender, EventArgs e)
+        {
+            //Show instructions on how to play the game
+            String title = "Instructions to play Craps";
+            String message = Environment.NewLine + "The player rolls 2 dice.  If the dice lands on 7 or 11. The player wins. " + Environment.NewLine + " " + Environment.NewLine + "If the dice land on 2, 3 or 12, the player loses." + Environment.NewLine + " " + Environment.NewLine + "If any other number comes up, like 9, that number becomes the point." + Environment.NewLine + " " + Environment.NewLine + "The player will next roll as many times as necessary to either hit their point, they win, or they hit a 7, they lose." + Environment.NewLine + " " + Environment.NewLine + "If any other number comes up, the player rolls again….until they hit their point or a 7." + Environment.NewLine + " " + Environment.NewLine + "Player will click on 'Play' only once, and the computer will roll the dices automatically as needed";
+            DialogResult resultabout = MessageBox.Show(message, title);
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            //Start a new game
+            Application.Restart();
+        }
+    }
+}
+
